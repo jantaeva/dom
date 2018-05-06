a42ab266a03e4e8fcd230ad771115753033eebcc
jantaeva <aizaada.jantaeva@gmail.com>
2018-05-06 08:40:12 +0600

    all commit

---
+++ igra.py
@@ -0,0 +1,9 @@
+print("ugaday chislo")
+answer=input("your answer?")
+print (answer)
+if (answer=="19.2"):
+    print("this answer is correct!!!")
+if(answer<"19.2"):
+    print("it is little number")
+if(answer>"19.2"):
+    print("it is bigger number ")
